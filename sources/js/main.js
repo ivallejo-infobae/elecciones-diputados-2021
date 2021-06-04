229eb5ef6e0fd5550f86b52e1082f6659f55ca66
Infobae Mexico <infobae@MacBook-Pro-Isaac.local>
2021-06-04 14:02:20 -0500

    update functionality buttons

--- sources/js/main.js
+++ sources/js/main.js
@@ -4,21 +4,29 @@ let botonesPartidos = $(".botones-partidos");
 
 let asientos = $(".asientos");
 
+let btnAll = $(".button-all");
+
 botonesPartidos.on('click', '.boton-partido', function(){
   let t = $(this);
   let data = t.attr('data');
   if(data == `${data}`) {
     asientos.find('img').each(function(){
+      
       let dataImg = $(this).attr('data');
-      if($(this).is(':visible')){
-        $(this).fadeOut().removeClass('open');
+      if(dataImg != `${data}`){
+        $(this).fadeOut().removeClass('on');
       }
-      // if($(this).hasClass('open')) {
-      //   $(this).fadeOut().removeClass('open');
-      // }
       if(dataImg == `${data}`) {
-        $(this).fadeIn().addClass('open')
+        $(this).fadeIn().addClass('on')
       }
     })
   }
+})
+
+btnAll.on('click', function(){
+  asientos.find('img').each(function(){
+    if(!$(this).is(':visible')){
+      $(this).fadeIn().removeClass('on');
+    }
+  })
 })
